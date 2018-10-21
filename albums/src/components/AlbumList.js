df17175e4633d60a8d9a0ba7717b43682212df05
Deni Junior <denidiasjr@gmail.com>
2018-10-21 16:33:07 -0300

    Start with axios and class based components

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import axios from 'axios';
+
+
+export default class AlbumList extends Component {
+
+    state = { albums: [] }
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums: response.data}));
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Minha lista por aqui</Text>
+            </View>
+        );
+    }
+
+}
