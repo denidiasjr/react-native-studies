074d6a40db9c9887a2a3f470b83b9c987c1cfee9
Deni Junior <denidiasjr@gmail.com>
2018-10-26 14:27:42 -0300

    Auth project

--- albums/src/components/CardSection.js
+++ albums/src/components/CardSection.js
@@ -13,7 +13,6 @@ const CardSection = (props) => {
 const styles = {
     containerStyles: {
         justifyContent: 'flex-start',
-        // borderBottomWidth: 1,
         padding: 5,
         backgroundColor: '#fff',
         flexDirection: 'row',
---
+++ auth/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    
+    return (
+        <View>
+            <Text>Se liga aí, que é hora da autenticação!</Text>
+        </View>
+    );
+};
+
+export default App;
