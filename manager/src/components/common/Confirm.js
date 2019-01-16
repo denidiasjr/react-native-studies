b29494a7169352f70b9206687cf3b92745fc7725
Deni Junior <denidiasjr@gmail.com>
2019-01-15 22:35:19 -0200

    Creating modal confirm

---
+++ manager/src/components/common/Confirm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    Modal
+} from 'react-native';
+import {
+    CardSection,
+} from './CardSection';
+import {
+    Button,
+} from './Button';
+
+const Confirm = (props) => {
+
+    return (
+        <Modal>
+        </Modal>
+    );
+    
+};
+
+export { Confirm };
